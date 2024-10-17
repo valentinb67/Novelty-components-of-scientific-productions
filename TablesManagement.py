9e2186ef2c0f8595f11fa8e2d76f9c2890c9fda4
Valentin Barthel <valentinbarthel17@gmail.com>
2024-10-17 15:54:14 +0200

    Add script
    Add script

--- TablesManagement.py
+++ TablesManagement.py
@@ -4,38 +4,41 @@ import ast
 import requests
 import time
 
-# Charger le fichier CSV
 DF_SDG_2 = pd.read_csv('DF_SDG.csv')
 
 #Unlist the columns 'authors'
-    #Convertir les chaînes de caractères en listes Python dans la colonne 'authors'
+    #Convert string to list in column 'authors'
 DF_SDG_2['authors'] = DF_SDG_2['authors'].apply(ast.literal_eval)
-    #Séparer les auteurs dans plusieurs colonnes
+    #Separate authors (one per column)
 authors_df = DF_SDG_2['authors'].apply(pd.Series)
-    #Renommer les colonnes de façon appropriée
+    #Rename columns
 authors_df.columns = [f'author_{i+1}' for i in range(authors_df.shape[1])]
-    #Combiner avec le dataframe original
+    #Combine with the main df
 DF_SDG_2 = pd.concat([DF_SDG_2, authors_df], axis=1)
 
 #Unlist the columns 'institutions'
-    # Convertir les chaînes de caractères en listes Python dans la colonne 'institutions'
+    #Convertir les chaînes de caractères en listes Python dans la colonne 'institutions'
 DF_SDG_2['institutions'] = DF_SDG_2['institutions'].apply(ast.literal_eval)
-    # Séparer les institutions dans plusieurs colonnes
+    #Separate the institutions
 institutions_df = DF_SDG_2['institutions'].apply(pd.Series)
-    # Renommer les colonnes de façon appropriée
+    #Rename the columns
 institutions_df.columns = [f'institution_{i+1}' for i in range(institutions_df.shape[1])]
-    # Combiner avec le dataframe original
+    #Combine the df
 DF_SDG_2 = pd.concat([DF_SDG_2, institutions_df], axis=1)
 
+
+print(DF_SDG_2)
+
+DF_SDG_2.to_csv("DF_SDG_2.csv", index=False)
 ########################################
 ############ DF_SDG_2_authors ############
-    #Filtrer les colonnes nécessaires : PMID, num_authors, Auteur_1 à Auteur_100
-selected_columns = ['PMID', 'num_authors', 'log_num_authors_SDG_2', 'log_num_authors_squared_SDG_2'] + [f'author_{i+1}' for i in range(100)]
-DF_SDG_2_authors_authors = DF_SDG_2[selected_columns]
-DF_SDG_2_authors_authors
+    #Filter the following columns: PMID, num_authors, Auteur_1 à Auteur_100
+#selected_columns = ['PMID', 'num_authors', 'log_num_authors_SDG_2', 'log_num_authors_squared_SDG_2'] + [f'author_{i+1}' for i in range(100)]
+#DF_SDG_2_authors_authors = DF_SDG_2[selected_columns]
+#DF_SDG_2_authors_authors
 
 ########################################
 ############ DF_SDG_2_metrics ############
 
-selected_columns_metrics = ['PMID', 'num_citations', 'Novelty']
-DF_SDG_2_metrics = DF_SDG_2[selected_columns_metrics]
+#selected_columns_metrics = ['PMID', 'num_citations', 'Novelty']
+#DF_SDG_2_metrics = DF_SDG_2[selected_columns_metrics]
